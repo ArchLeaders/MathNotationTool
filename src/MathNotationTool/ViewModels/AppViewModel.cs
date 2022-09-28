7e891cf0a8f25d6b41246cf5a0c794364df6cc30
ArchLeaders <archparliament@gmail.com>
2022-09-28 11:24:57 -0700

    Create Version string field for window title

--- src/MathNotationTool/ViewModels/AppViewModel.cs
+++ src/MathNotationTool/ViewModels/AppViewModel.cs
@@ -1,10 +1,17 @@
 ﻿using Avalonia.Controls;
+using MathNotationTool.Views;
 using System;
 
 namespace MathNotationTool.ViewModels
 {
     public class AppViewModel : ReactiveObject
     {
+        private string version = "Math Notation Tool - v0.0.4-alpha"; // Use compile time keys for version?
+        public string Version {
+            get => version;
+            set => this.RaiseAndSetIfChanged(ref version, value);
+        }
+
         private bool isMaximized = false;
         public bool IsMaximized {
             get => isMaximized;
