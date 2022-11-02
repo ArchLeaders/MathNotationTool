c45fe8edd7002a0c8ebe54100ee717418fe9229c
ArchLeaders <archparliament@gmail.com>
2022-11-02 14:47:24 -0700

    Init `TextMate.Grammars`

--- src/Views/AppView.axaml.cs
+++ src/Views/AppView.axaml.cs
@@ -1,4 +1,10 @@
 using Avalonia.Controls;
+using AvaloniaEdit.TextMate;
+using AvaloniaEdit;
+using AvaloniaEdit.TextMate.Grammars;
+using Avalonia.Themes.Fluent;
+using System;
+using System.Diagnostics;
 
 namespace MathNotationTool.Views
 {
@@ -7,6 +13,17 @@ namespace MathNotationTool.Views
         public AppView()
         {
             InitializeComponent();
+
+            App.Theme.ObservableForProperty(x => x.Mode).Subscribe(x => InitTextEditor(x.Value));
+            InitTextEditor(App.Theme.Mode);
+        }
+
+        public void InitTextEditor(FluentThemeMode theme)
+        {
+            RegistryOptions registryOptions = theme == FluentThemeMode.Dark ? new(ThemeName.DarkPlus) : new(ThemeName.LightPlus);
+            TextMate.Installation textMateInstallation = editor.InstallTextMate(registryOptions);
+            Language csharpLanguage = registryOptions.GetLanguageByExtension(".c");
+            textMateInstallation.SetGrammar(registryOptions.GetScopeByLanguageId(csharpLanguage.Id));
         }
     }
 }
