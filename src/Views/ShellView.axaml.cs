76f09c8ee0ace2a8673dfeaa441f1163f6cf7f23
ArchLeaders <archparliament@gmail.com>
2022-11-05 12:28:38 -0700

    Use standard win-x64 window rules

--- src/Views/ShellView.axaml.cs
+++ src/Views/ShellView.axaml.cs
@@ -10,22 +10,5 @@ namespace MathNotationTool.Views
         {
             InitializeComponent();
         }
-
-        // Fix Win32 clipping issues
-        protected override void HandleWindowStateChanged(WindowState state)
-        {
-            if (state == WindowState.Maximized) {
-                Padding = new Thickness(5);
-                ExtendClientAreaTitleBarHeightHint = 104;
-                ShellContext.IsMaximized = true;
-            }
-            else {
-                Padding = new Thickness(8);
-                ExtendClientAreaTitleBarHeightHint = 90;
-                ShellContext.IsMaximized = false;
-            }
-
-            base.HandleWindowStateChanged(state);
-        }
     }
 }
