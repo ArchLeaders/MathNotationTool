4af890185017a3968d79971991000ee7217e5691
ArchLeaders <archparliament@gmail.com>
2022-11-05 12:28:17 -0700

    Handle errors at the application root

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.ReactiveUI;
+using AvaloniaGenerics.Dialogs;
 using System;
 
 namespace MathNotationTool
@@ -11,8 +12,15 @@ namespace MathNotationTool
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
         [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+        public static void Main(string[] args)
+        {
+            try {
+                BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+            }
+            catch (Exception ex) {
+                MessageBox.ShowSync(ex.ToString(), "Unhandled Exception", formatting: Formatting.Markdown);
+            }
+        }
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
