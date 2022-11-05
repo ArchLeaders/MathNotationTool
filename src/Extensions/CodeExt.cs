01d18a91eaf3dfff159045166acdbc09b18b1e39
ArchLeaders <archparliament@gmail.com>
2022-11-05 14:52:52 -0700

    Linq query for HistoryItem name

--- src/Extensions/CodeExt.cs
+++ src/Extensions/CodeExt.cs
@@ -10,14 +10,16 @@ namespace MathNotationTool.Extensions
 {
     public static class CodeExt
     {
+        public static decimal Query(string id)
+        {
+            return ViewModel.Calculator.ViewModel.History.Where(x => x.Name == id).FirstOrDefault()?.Value ??
+                throw new KeyNotFoundException($"Could not find entry '{id}' in Calculator History.");
+        }
+
         public static decimal Query(int index)
         {
-            try {
-                return ViewModel.Calculator.ViewModel.History[index-1].Value;
-            }
-            catch {
+            return ViewModel.Calculator.ViewModel.History.Where(x => x.Name == index.ToString()).FirstOrDefault()?.Value ??
                 throw new KeyNotFoundException($"Could not find entry '{index}' in Calculator History.");
-            }
         }
     }
 }
